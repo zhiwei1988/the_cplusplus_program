58bcb2c6a036b166ac5b26f0db07e28ad5a5c047
Zhiwei Tan <zhiweix1988@gmail.com>
2022-02-21 19:09:03 +0800

    add scope_thread

--- src/create_thread.cpp
+++ src/create_thread.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <pthread.h>
+#include <utility>
 
 void helloFunction()
 {
@@ -15,6 +16,27 @@ public:
     }
 };
 
+class scoped_thread {
+public:
+    explicit scoped_thread(std::thread t_): t(std::move(t_))
+    {
+        if (!t.joinable()) 
+            throw std::logic_error("No thread");
+    }
+    
+    ~scoped_thread() 
+    {
+        t.join();
+    }
+
+    scoped_thread(scoped_thread&) = delete;
+    
+    scoped_thread& operator=(scoped_thread const &) = delete;
+    
+private:
+    std::thread t;
+};
+
 int main()
 {
 
@@ -26,11 +48,10 @@ int main()
     std::thread t2(helloFunctionObject);
 
     std::thread t3([]{std::cout << "Hello from a lambda." << std::endl;});
+    
+    scoped_thread st1(std::move(t1));
+    scoped_thread st2(std::move(t2));
+    scoped_thread st3(std::move(t3));
 
-    t1.join();
-    t2.join();
-    t3.join();
-
-    std::cout << std::endl;
-
+    std::cout << "the end" << std::endl;
 };
